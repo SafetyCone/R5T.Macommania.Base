76e23fa2b3fcc53b9ed9333c8d1ba60a091abfcd
MinexAutomation <minex.automation@gmail.com>
2019-10-23 21:52:18 -0700

    Added documentation.

--- source/R5T.Macommania.Base/Code/Services/Interfaces/IExecutableFilePathProvider.cs
+++ source/R5T.Macommania.Base/Code/Services/Interfaces/IExecutableFilePathProvider.cs
@@ -3,6 +3,9 @@
 
 namespace R5T.Macommania
 {
+    /// <summary>
+    /// Provides the path of the currently executing executable file.
+    /// </summary>
     public interface IExecutableFilePathProvider
     {
         string GetExecutableFilePath();
