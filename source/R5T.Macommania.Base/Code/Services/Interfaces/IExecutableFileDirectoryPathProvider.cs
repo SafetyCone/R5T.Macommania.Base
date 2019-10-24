0ef11cc000a59dcf6b5551e5c7db12a921149fc0
MinexAutomation <minex.automation@gmail.com>
2019-10-23 21:53:02 -0700

    Added service interface providing the directory path of the currently executing executable file.

---
+++ source/R5T.Macommania.Base/Code/Services/Interfaces/IExecutableFileDirectoryPathProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace R5T.Macommania
+{
+    /// <summary>
+    /// Provides the parent directory path of the current executing executable file.
+    /// </summary>
+    public interface IExecutableFileDirectoryPathProvider
+    {
+        string GetExecutableFileDirectoryPath();
+    }
+}
