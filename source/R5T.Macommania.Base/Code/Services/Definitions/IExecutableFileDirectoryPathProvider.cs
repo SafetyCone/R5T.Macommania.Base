034852a0fffacb3795d19b479edafe454e1409c1
davidcoats <david@getsafetycone.com>
2022-04-27 15:32:38 -0700

    Interim changes.

---
+++ source/R5T.Macommania.Base/Code/Services/Definitions/IExecutableFileDirectoryPathProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using R5T.T0064;
+
+
+namespace R5T.Macommania
+{
+    /// <summary>
+    /// Provides the parent directory path of the current executing executable file.
+    /// </summary>
+    [ServiceDefinitionMarker]
+    public interface IExecutableFileDirectoryPathProvider : IServiceDefinition
+    {
+        string GetExecutableFileDirectoryPath();
+    }
+}
---
+++ source/R5T.Macommania.Base/Code/Services/Definitions/IExecutableFilePathProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using R5T.T0064;
+
+
+namespace R5T.Macommania
+{
+    /// <summary>
+    /// Provides the path of the currently executing executable file.
+    /// </summary>
+    [ServiceDefinitionMarker]
+    public interface IExecutableFilePathProvider : IServiceDefinition
+    {
+        string GetExecutableFilePath();
+    }
+}
